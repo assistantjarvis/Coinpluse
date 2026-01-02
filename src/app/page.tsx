62d0189ac688d264669dd3b0de6ebcd9e74f0701
Chetan Mishra <mishravineet29@gmail.com>
2026-01-02 23:07:06 +0530

    Add DataTable component with UI table and fix TypeScript errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import DataTable from "@/components/DataTable";
 
 const page = () => {
-  return <p className='text-3xl text-indigo-500'>Coinpluse</p>
-}
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <div id="coin-overview">
+          <div className="header pt-2">
+            <Image
+              src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
+              alt="Bitcoin Image"
+              width={56}
+              height={56}
+            />
+            <div className="info">
+              <p>Bitcoin / BTC</p>
+              <h1> $89,113.00</h1>
+            </div>
+          </div>
+        </div>
+        <p> Trending Coins</p>
+        <DataTable />
+      </section>
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  );
+};
 
-export default page
+export default page;
